bf37b720829867914a48286497fe613f7c24f1cc
Bolshakov Stanislav <bolshestas@gmail.com>
2021-07-22 18:18:17 +0300

    optimalversion

--- djangovue/frontend/src/App.vue
+++ djangovue/frontend/src/App.vue
@@ -1,26 +1,242 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to  Django and Your Vue.js App"/>
+<div id="app">
+<div class="named">
+<div class="container" style="padding-top: 100px; padding-bottom: 100px; color: white; text-align: center;">
+ <h1>Lorem, ipsum.</h1>
+ <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore ipsam adipisci atque nobis cum magni voluptas, ducimus, praesentium quibusdam tenetur saepe repudiandae asperiores repellendus consectetur doloribus architecto ratione dolore laboriosam ullam dicta delectus quis. Mollitia at, ratione eos non, saepe fugit recusandae quidem distinctio soluta dignissimos ducimus ullam aliquam similique expedita quae dolorem ad velit quo culpa quia, ex assumenda fuga numquam eaque. Vero, dolorum? Aperiam labore excepturi hic nihil consequuntur, beatae corrupti dignissimos veritatis? Nostrum libero natus quos! Ipsum voluptatum autem molestias incidunt a nisi exercitationem quod itaque ipsam repellendus voluptas distinctio harum sit, dolorem, vel rerum sint aspernatur?</p>
+</div>
+<div class="container marketing">
+<div class="row">
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" role="img" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+        <h2>Заголовок</h2>
+        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">Подробнее »</a></p>
+      </div>
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" role="img" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+        <h2>Заголовок</h2>
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">Подробнее »</a></p>
+      </div>
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" role="img" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+        <h2>Заголовок</h2>
+        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">Подробнее »</a></p>
+      </div>
+    </div>
+
+    
+    </div>
+  </div>
+
+
+<div class="full-width-testimonial">
+  <div class="full-width-testimonial-section">
+    <div class="full-width-testimonial-icon text-center">
+      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+         width="41px" height="400px" viewBox="-235 240 41 34" style="enable-background:new -235 240 41 34;" xml:space="preserve">
+          <path class="quote-path" d="M-231.3,260.4c0-5,1.3-8.8,3.7-11.7c2.4-2.8,6-4.6,10.5-5.5v5c-3.5,1-6,2.8-7.1,5.5c-0.7,1.4-0.9,2.8-0.8,4
+            h8.1v12.8h-14.4V260.4z"/>
+          <path class="quote-path" d="M-212,260.4c0-5,1.3-8.8,3.7-11.7c2.4-2.8,6-4.6,10.5-5.5v5c-3.5,1-6,2.8-7.1,5.5c-0.7,1.4-0.9,2.8-0.8,4h8.1
+            v12.8H-212V260.4z"/>
+      </svg>
+    </div>
+    <div class="full-width-testimonial-content">
+      <h5 class="full-width-testimonial-text">
+      Lorem ipsum dolor sit, amet consectetur 
+      adipisicing elit. Officiis, hic. Animi esse 
+      ea quaerat perferendis repudiandae, consequatur nisi unde veniam laudantium fugit 
+      numquam quisquam nihil iusto nam distinctio 
+      voluptas in est. Corporis cum et, nostrum nulla eaque a officia atque quas vero perspiciatis excepturi praesentium temporibus
+      eius asperiores provident voluptas id nemo fugit quisquam. Veritatis quaerat necessitatibus vitae incidunt nihil eos adipisci.
+      </h5>
+      <p class="full-width-testimonial-source">Lorem ipsum dolor sit amet.</p>
+      <span class="full-width-testimonial-source-context">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, facere!</span>
+    </div>
+  </div>
+</div>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.named {
+    width: 100%;
+    background: linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.1)), url("https://backgroundcheckall.com/wp-content/uploads/2017/12/color-block-background-8.jpg");
+    background-position: center center;
+}
+
+.full-width-testimonial {
+    background: linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.1)), url("https://unsplash.it/2000/1200/?image=1011");
+    background-position: center center;
+  }
+  
+.full-width-testimonial-icon .quote-path {
+    fill: none;
+    stroke: #ffae00;
+    stroke-width: 2.5192;
+    stroke-miterlimit: 10;
+}
+  
+.full-width-testimonial-content,
+.full-width-testimonial-icon {
+    text-align: center;
+}
+
+  
+.full-width-testimonial-section .full-width-testimonial-text {
+    font-weight: 300;
+    color: #ffffff;
+    padding-left: 30px;
+    padding-right: 30px;
+
+    padding-left: 50px;
+    padding-right: 50px;
+    font-size: 1.5em;
+}
+  
+@media print, screen and (min-width: 40em) {
+.full-width-testimonial-section .full-width-testimonial-text {
+      padding-left: 100px;
+      padding-right: 100px;
+    }
+}
+  
+@media print, screen and (min-width: 64em) {
+.full-width-testimonial-section .full-width-testimonial-text {
+      padding-left: 180px;
+      padding-right: 180px;
+      font-size: 1.8em;
+    }
+}
+  
+@media screen and (min-width: 75em) {
+.full-width-testimonial-section .full-width-testimonial-text {
+      padding-left: 300px;
+      padding-right: 300px;
+    }
+}
+  
+.full-width-testimonial-section .full-width-testimonial-source {
+    font-size: 1.2em;
+    font-weight: 600;
+    color: #ffffff;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    padding-bottom: 0;
+    margin-bottom: 0.2em;
+    font-size: 1.0em;
+}
+  
+.full-width-testimonial-section .full-width-testimonial-source-context {
+    display: block;
+    font-size: 0.9em;
+    font-weight: 500;
+    color: #ffffff;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    font-size: 0.8em;
+}
+  
+.full-width-testimonial-section .slick-slider {
+    margin-bottom: 0px;
+}
+  
+.full-width-testimonial-text,
+.full-width-testimonial-content,
+.full-width-testimonial-source,
+.full-width-testimonial-icon {
+    padding-bottom: 2rem;
+}
+  
+.full-width-testimonial-content {
+    width: 100%;
+}
+
+* {
+    padding: 0;
+    margin: 0;
+    font-family: sans-serif;
+}
+
+#sidebar.active {
+    left: 20px;
+}
+
+#sidebar {
+    position: fixed;
+    width: 250px;
+
+    background: #57626f;
+    border-right: 4px solid #E0E5EA;
+    left: -260px;
+    padding-bottom: 10px;
+    top: 10px;
+    border-radius: 10px;
+
+    -moz-transition: all 300ms;
+    -webkit-transition: all 600ms;
+    transition: all 600ms;
+}
+
+#sidebar ul li:first-child {
+    text-align: center;
+    color: white;
+    font-size: 1.95em;
+    list-style: none;
+    padding: 25px 10px 10px 10px;
+}
+
+#sidebar ul li:not(:first-child) {
+    width: 90%;
+    margin-left: 10%;
+    font-size: 1.1em;
+    list-style: none;
+    padding: 15px 10px;
+}
+
+#sidebar ul li a {
+    color: #dfe5eb;
+    text-decoration: none;
+}
+
+#sidebar ul li a:hover {color: #59e9df}
+
+#sidebar .toggle-btn {
+    position: absolute;
+    left: 270px;
+    top: 20px;
+    background: #e0e5ea;
+    padding: 10px;
+    border-radius: 12px;
+    cursor: pointer;
+}
+
+#sidebar .toggle-btn:hover {background: #556270;}
+
+#sidebar .toggle-btn:hover span {background: #fafafa;}
+
+#sidebar .toggle-btn span {
+    display: block;
+    width: 25px;
+    height: 4px;
+    background-color: #666;
+    margin: 4px 0;
 }
 </style>
