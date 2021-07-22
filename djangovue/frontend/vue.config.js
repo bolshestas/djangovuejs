40e951fcc02edf1b6a36de85e78057a95c3684dc
Bolshakov Stanislav <bolshestas@gmail.com>
2021-07-22 03:07:01 +0300

    create vuejs and django project

---
+++ djangovue/frontend/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/static/dist/' : 'http://localhost:8080',
+    outputDir: '../backend/static/dist',
+    indexPath: '../../templates/base-vue.html',
+
+    chainWebpack: config => {
+        config.devServer
+        .public('http://localhost:8080')
+        .hotOnly(true)
+        .headers({'Acces-Control-Allow-Origin': '*'})
+        .writeToDisk(filePath => filePath.endsWith('index.html'))
+    }
+}
